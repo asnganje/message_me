91d89dab2ac678ee152fdcd6efce7f4ebe1b0eca
asnganje <abd.nganje@gmail.com>
2025-04-22 13:50:21 +0300

    add basic actions for sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,13 @@
 class SessionsController < ApplicationController
+  # def new
+  #   @user = User.new
+  # end
+  # def create
+  #   @user = User.new(user_params)
+  # end
+
+  private
+  def user_params
+    params.require(:user).permit(:username, :password_digest)
+  end
 end
