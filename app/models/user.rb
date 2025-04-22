d7606a11fdb6dcbcd1fa24ddc81cfcf089136973
asnganje <abd.nganje@gmail.com>
2025-04-22 13:51:07 +0300

    add user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   validates :username,
             uniqueness: true,
             presence: true,
-            length: {minimum:3, maximum:15}
+            length: { minimum: 3, maximum: 15 }
   has_many :messages
   has_secure_password
 end
