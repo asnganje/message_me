28d3304a10fd40d3be9c2a23b0990c2a43f0abd9
asnganje <abd.nganje@gmail.com>
2025-04-22 07:44:52 +0300

    add message resource

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,6 @@ class User < ApplicationRecord
             uniqueness: true,
             presence: true,
             length: {minimum:3, maximum:15}
+  has_many :messages
   has_secure_password
 end
